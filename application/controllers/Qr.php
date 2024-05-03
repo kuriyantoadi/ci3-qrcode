6b46057bb95a54ceddab80e706734b73c1b65d34
kuriyantoadi <melatikuning212@gmail.com>
2024-05-03 13:46:37 +0700

    upload ci3-qrcode

---
+++ application/controllers/Qr.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Qr extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->library('ciqrcode');
+        $this->load->helper('url');
+    }
+
+    public function index() {
+        $qr['data'] = 'http://h4nk.blogspot.com/2016/11/generate-qr-code-dengan-codeigniter.html';
+        $qr['level'] = 'H';
+        $qr['size'] = 2;
+        $qr['savename'] = FCPATH.'qr.png';
+        $this->ciqrcode->generate($qr);
+
+        $data['qr_image'] = base_url().'qr.png'; // Mengirim URL gambar ke view
+        $this->load->view('barcode_view', $data); // Memuat view untuk menampilkan gambar QR
+    }
+}
